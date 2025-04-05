4a54e21ade595c012b43423cfad8079a6663cf87
Artem Setsko <meltenartsyt@gmail.com>
2025-04-05 16:10:09 +0300

    restore all changes

--- ReactProject/src/middleware/adminCheck.js
+++
@@ -1,9 +0,0 @@
-export const adminCheck = (req, res, next) => {
-    if (req.user.role !== 'admin') {
-      return res.status(403).json({
-        success: false,
-        message: 'Доступ запрещен. Требуются права администратора'
-      });
-    }
-    next();
-  };
